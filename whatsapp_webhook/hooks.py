49fe6c2a38983c1afa0545c396e30469a6e2a1f7
Batsy69 <99815028+Batsy69@users.noreply.github.com>
2025-05-02 17:48:06 +0530

    Update hooks.py

--- whatsapp_webhook/hooks.py
+++ whatsapp_webhook/hooks.py
@@ -1,1 +1,10 @@
+# whatsapp_webhook/hooks.py
+
+app_name = "whatsapp_webhook"
+app_title = "Whatsapp Webhook"
+app_publisher = "Yusuf Paloba"
+app_description = "Handle WhatsApp webhooks via Meta"
+app_email = "yusufpaloba43@outlook.com"
+app_license = "MIT"
+
 has_route = True
