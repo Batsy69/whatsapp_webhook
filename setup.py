9c70dd20929997a00bc6584b0fc8df704dc8fe13
Batsy69 <99815028+Batsy69@users.noreply.github.com>
2025-05-03 19:25:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='whatsapp_webhook',
     version='0.0.1',
-    description='Webhook for WhatsApp Meta API',
+    description='Handle WhatsApp webhooks via Meta',
     author='Yusuf Paloba',
     author_email='yusufpaloba43@outlook.com',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=['frappe'],
+    install_requires=[]
 )
